0e15574cad1857a8e5c7bc316917dfeb89b4f0d2
hassan tarif <hassantarif4@gmail.com>
2021-11-06 15:03:32 +0100

    init

---
+++ game.rb
@@ -0,0 +1,22 @@
+class Game
+    def initialize
+            # Call the board 
+            # Call the Display function
+            # Call the players
+            # Call the Current player and Give it away to play with his cursor
+    end
+    def play
+        # let the player play and notify it and swap turns!
+    end
+
+    private 
+
+    def notify_players
+        # notify the player of his turn that his now ready to play because the other players play his turn
+    end
+
+    def swap_turn!
+        # swap turn for both the player
+        # write a function to give the player a turn in 60 seconds when the players finish thinking and chosing the right row
+    end
+end
